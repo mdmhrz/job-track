6268f2514e8519a2637d310d72d9efd369eea14c
mdmhrz <razufreelance@gmail.com>
2025-06-13 18:44:03 +0600

    Show all button done

--- src/components/TopCompanies.jsx
+++ src/components/TopCompanies.jsx
@@ -26,7 +26,7 @@ const TopCompanies = () => {
                         </p>
                     </div>
 
-                    <div className="grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-6 md:gap-8">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6 md:gap-8">
                         {visibleCompanies.map(company => (
                             <Company key={company.id} company={company} />
                         ))}
